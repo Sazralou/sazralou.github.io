f63afd8f9abfc93190ad2422cd3472b444ab9aa2
Sazralou <65719195+Sazralou@users.noreply.github.com>
2020-06-07 17:33:37 +0100

    Update index.html file with own files

--- dlc/index.js
+++ dlc/index.js
@@ -1,31 +1,7 @@
 [
     { 
-        "file": "context.dlc", 
-        "title": "Context Logo",
+        "file": "tu003150.dlc", 
+        "title": "Not sure",
         "buttons": [
-            { "title": "Logo", "action": [75,0,4,4] }
+            { "title": "Not sure", "action": [75,0,4,4] }
         ]
-    },
-    { 
-        "file": "hacked.dlc", 
-        "title": "HACKED",
-        "buttons": [
-            { "title": "Hacked 1", "action": [75,0,3,4] },
-            { "title": "Hacked 2", "action": [75,0,4,4] }
-        ]
-    },
-    {
-        "file": "noimg2.dlc", 
-        "title": "Audio only",
-        "buttons": [
-            { "title": "Audio test ", "action": [75,0,4,4] }
-        ]
-    },
-    {
-        "file": "chilli.dlc", 
-        "title": "Chilli",
-        "buttons": [
-            { "title": "Chilli ", "action": [75,0,4,4] }
-        ]
-    }
-]
