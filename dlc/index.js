362702d8fa0d4f1f55597e1a3bc00f1227d94218
Sazralou <65719195+Sazralou@users.noreply.github.com>
2020-06-07 17:40:18 +0100

    Update index.js

--- dlc/index.js
+++ dlc/index.js
@@ -3,5 +3,28 @@
         "file": "tu003150.dlc", 
         "title": "Not sure",
         "buttons": [
-            { "title": "Not sure", "action": [75,0,4,4] }
+            { "title": "Note sure", "action": [75,0,4,4] }
         ]
+    },
+    { 
+        "file": "hacked.dlc", 
+        "title": "HACKED",
+        "buttons": [
+            { "title": "Hacked 1", "action": [75,0,3,4] },
+        ]
+    },
+    {
+        "file": "noimg2.dlc", 
+        "title": "Audio only",
+        "buttons": [
+            { "title": "Audio test ", "action": [75,0,4,4] }
+        ]
+    },
+    {
+        "file": "chilli.dlc", 
+        "title": "Chilli",
+        "buttons": [
+            { "title": "Chilli ", "action": [75,0,4,4] }
+        ]
+    }
+]
